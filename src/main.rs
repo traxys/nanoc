17ffc40f3e3ed21f2285f055f2f771c6ef50a9a1
Quentin Boyer <qbsecond@gmail.com>
2020-02-10 14:40:49 +0100

    functions

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
+#![allow(unused_parens)]
 use logos::Logos;
 
-#[macro_use] extern crate lalrpop_util;
+#[macro_use]
+extern crate lalrpop_util;
+
 lalrpop_mod!(pub nanolang);
 
 #[derive(Logos, Debug, Copy, Clone)]
@@ -31,6 +34,8 @@ enum TokenKind {
     RBrace,
     #[token = ";"]
     Semicolon,
+    #[token = "fn"]
+    Function,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -46,6 +51,7 @@ pub enum Token<'i> {
     Equals,
     Ident(&'i str),
     Semicolon,
+    Function,
 }
 impl<'i> Token<'i> {
     fn from_kind(kind: TokenKind, input: &'i str) -> Self {
@@ -59,6 +65,7 @@ impl<'i> Token<'i> {
             TokenKind::LBrace => Self::LBrace,
             TokenKind::RParen => Self::RParen,
             TokenKind::LParen => Self::LParen,
+            TokenKind::Function => Self::Function,
             TokenKind::Placeholder => Self::Placeholder,
             TokenKind::Int => Self::Int(input.parse().expect("int is not an int")),
             invalid_kind => panic!("kind can't become a token: {:?}", invalid_kind),
@@ -110,13 +117,64 @@ impl<'i> Iterator for Lexer<'i> {
 }
 
 #[derive(Debug)]
+pub enum Param<'i> {
+    Expr(Expr<'i>),
+    Placeholder,
+}
+
+pub fn desugar_call<'i>(func: Expr<'i>, args: Vec<Param<'i>>) -> Expr<'i> {
+    let mut new_args = Vec::new();
+    let mut params = Vec::new();
+    let mut current = 0;
+    for arg in args {
+        match arg {
+            Param::Expr(e) => new_args.push(e),
+            Param::Placeholder => {
+                params.push(Binding::Unamed(current));
+                new_args.push(Expr::Unamed(current));
+                current += 1;
+            }
+        }
+    }
+    let call = Expr::Call {
+        func: Box::new(func),
+        args: new_args,
+    };
+    if params.is_empty() {
+        call
+    } else {
+        Expr::FuncDefinition {
+            name: None,
+            bindings: params,
+            body: Box::new(call),
+        }
+    }
+}
+
+#[derive(Debug)]
+pub enum Binding<'i> {
+    Named(&'i str),
+    Unamed(isize),
+}
+
+#[derive(Debug)]
 pub enum Expr<'i> {
+    Unamed(isize),
     Ident(&'i str),
     Int(i64),
+    Call {
+        func: Box<Expr<'i>>,
+        args: Vec<Expr<'i>>,
+    },
+    FuncDefinition {
+        name: Option<&'i str>,
+        bindings: Vec<Binding<'i>>,
+        body: Box<Expr<'i>>,
+    },
 }
 
 fn main() {
-    let input = "(42)";
+    let input = "fn id (i) -> {i}(_)(_)()";
     let lexer = Lexer::new(input);
     println!("{:?}", nanolang::ExprParser::new().parse(input, lexer));
 }
